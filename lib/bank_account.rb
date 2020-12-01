0b404707c04ff118344ddeb51df50c018f020120
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-12-01 19:55:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,21 +1,21 @@
 class BankAccount
   attr_accessor :balance, :status
     attr_reader :name
-  
+
     def initialize(name)
       @name = name
       @balance = 1000
       @status = "open"
     end
-  
+
     def deposit(amount)
       @balance += amount
     end
-  
+
     def display_balance
       "Your Balance is $#{@balance}."
     end
-  
+
     def valid?
       if @status == "open" && @balance > 0
         true
@@ -23,10 +23,10 @@ class BankAccount
         false
       end
     end
-  
+
     def close_account
       @status = "closed"
     end
-  
+
   end
 end
